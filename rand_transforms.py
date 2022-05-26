c0a80b1bfedbb53aa2067e13bb2055c76fbffd1d
eva_n <eva.natinsky@utexas.edu>
2022-05-26 13:55:14 -0600

    new data + prep

--- rand_transforms.py
+++ rand_transforms.py
@@ -1,42 +1,143 @@
+import PIL.Image
 import numpy as np
-import torch as t
-import torchvision.io as tv
+import random
 import torchvision.transforms as trf
+from torchvision.transforms import functional as TVF
 import PIL.Image as Image
 import os
+from tqdm import tqdm
 
-crop_size = 128  # image square crop size
-crop_trf = trf.RandomCrop(crop_size)
-# transform image with rotation up to indicated degrees, a horizontal flip, and a vertical flip. Each transform has a p chance of occurring.
-rand_trf = trf.RandomApply([trf.RandomRotation(70),
-                            trf.RandomHorizontalFlip(),
-                            trf.RandomVerticalFlip()], p=0.8)
 
-path = "new-png-conversions"  # relative image folder path
-total_images = 1000  # total number of images to create
+def random_trf(image: PIL.Image.Image, min_dim=None, target=None):
+    """ Applies random transformations to a single image or pair for data augmentation.
+    :param image: Independent image, or source image if using pairs.
+    :param min_dim: Minimum dimension of input image(s). For transforming sets of differently sized images, locks size of output images.
+    :param target: Corresponding target in an image pair.
+    """
+    P = image.size[0]
+    if min_dim is None:
+        min_dim = P
+    rng = np.random.default_rng()
+    angle = rng.integers(0, 270)  # get random angle in degrees
+    rad_angle = np.radians(angle)
+    c_crop = int(P / (np.abs(np.sin(rad_angle)) + np.abs(np.cos(rad_angle))))  # get bbox size based on rotation
+    min_crop = int(min_dim / (2 * np.cos(np.pi / 4)))  # get smallest bbox
+    temp_source = trf.CenterCrop(c_crop)(image.rotate(angle))  # rotate and crop to valid data
 
-if os.path.isdir(path):
-    save_folder = os.path.join(path, 'new_ml_data')  # saves into folder called "ml_data"
-    if not os.path.isdir(save_folder):
-        os.mkdir(save_folder)
+    if target is None:  # for augmenting unpaired images
+        transformer = trf.Compose([trf.RandomCrop(min_crop), trf.RandomHorizontalFlip(), trf.RandomVerticalFlip()])
+        new_source = transformer(temp_source)
+        return new_source
+
+    else:  # for augmenting image pairs with the same transformations
+        temp_target = trf.CenterCrop(c_crop)(target.rotate(angle))  # rotate and crop to valid data
+        c_top = random.randint(0, (c_crop - min_crop))
+        c_left = random.randint(0, (c_crop - min_crop))
+        flips = random.choice(['h', 'v', 'both', 'none'])
+
+        # flips and crops
+        if flips == 'h':
+            new_source = TVF.crop(TVF.hflip(temp_source), c_top, c_left, min_crop, min_crop)
+            new_target = TVF.crop(TVF.hflip(temp_target), c_top, c_left, min_crop, min_crop)
+        elif flips == 'v':
+            new_source = TVF.crop(TVF.vflip(temp_source), c_top, c_left, min_crop, min_crop)
+            new_target = TVF.crop(TVF.vflip(temp_target), c_top, c_left, min_crop, min_crop)
+        elif flips == 'both':
+            new_source = TVF.crop(TVF.hflip(TVF.vflip(temp_source)), c_top, c_left, min_crop, min_crop)
+            new_target = TVF.crop(TVF.hflip(TVF.vflip(temp_target)), c_top, c_left, min_crop, min_crop)
+        else:  # flips == 'none'
+            new_source = TVF.crop(temp_source, c_top, c_left, min_crop, min_crop)
+            new_target = TVF.crop(temp_target, c_top, c_left, min_crop, min_crop)
+
+        return new_source, new_target
+
+
+def augment(in_path, out_path, total_imgs, min_px=None):
+    """ Augments a set of independent images (unpaired).
+
+    """
+    if os.path.isdir(in_path):
+        if not os.path.isdir(out_path):
+            os.mkdir(out_path)
+        pbar = tqdm(total=total_imgs, unit='file', desc='Augmenting data', leave=True)  # progress bar
+        index = 0
+        while index < total_imgs:
+            with os.scandir(in_path) as folder:
+                for item in folder:
+                    name, ext = os.path.splitext(item.name)
+                    if ext.lower() not in {'.png'}:  # image extensions must be in this set, other items are skipped
+                        continue
+                    with Image.open(item.path) as im:
+                        im.convert('RGB')
+                        im.load()
+                    if min_px is None:
+                        min_px = im.size[0]
+                    transformed_image = random_trf(im, min_px)
+                    save_name = name + str(index) + ext
+                    save_path = os.path.join(out_path, save_name)  # image name with index
+                    transformed_image.save(save_path)
+                    pbar.update(1)
+                    index += 1
+                    if index > total_imgs:
+                        break
+        pbar.close()
+    else:
+        raise AssertionError("Input path is not a directory!")
+
+
+def augment_pairs(source_path_in, source_path_out, target_path_in, target_path_out, total_imgs, min_px):
+    assert (os.path.isdir(source_path_in) and os.path.isdir(target_path_in)), "One of your input paths is not a directory!"
+    if not os.path.isdir(source_path_out):
+        os.mkdir(source_path_out)
+    if not os.path.isdir(target_path_out):
+        os.mkdir(target_path_out)
+
+    pbar = tqdm(total=total_imgs, unit='file', desc='Augmenting data', leave=True)  # progress bar
     index = 0
-    while index < total_images:
-        with os.scandir(path) as folder:
-            for item in folder:
-                name, ext = os.path.splitext(item)
-                if ext.lower() not in {'.png'}:  # image extensions must be in this set, other items are skipped
-                    continue
-                image_path = item.path
-                torch_image = tv.read_image(image_path,
-                                            mode=tv.ImageReadMode.RGB)
-                transformed_image = rand_trf(torch_image)
-                # final_image = crop_trf(transformed_image)
-                final_image = transformed_image  # for no cropping
-                image_name = str(index) + '_calib_HS20MG.png'
-                save_path = os.path.join(save_folder, image_name)  # image name with index
-                tv.write_png(final_image, save_path,
-                             compression_level=0)
-                print("Saved image", image_name)
-                index += 1
-                if index > total_images:
-                    break
+    while index < total_imgs:
+        s_folder = sorted(os.scandir(source_path_in), key=lambda e: e.name)
+        t_folder = sorted(os.scandir(target_path_in), key=lambda e: e.name)
+        for (s, t) in zip(s_folder, t_folder):
+            s_name, ext = os.path.splitext(s.name)
+            t_name = os.path.splitext(t.name)[0]
+            if ext.lower() not in {'.png'}:  # image extensions must be in this set, other items are skipped
+                continue
+            with Image.open(s.path) as source:  # load source image
+                source.convert('RGB')
+                source.load()
+            with Image.open(t.path) as target:  # load target image
+                target.convert('RGB')
+                target.load()
+            if min_px is None:  # get size from source if not specified
+                min_px = s.size[0]
+
+            transformed_source, transformed_target = random_trf(source, min_px, target)
+
+            source_save_name = s_name + str(index) + ext
+            target_save_name = t_name + str(index) + ext
+            source_save_path = os.path.join(source_path_out, source_save_name)  # image name with index
+            target_save_path = os.path.join(target_path_out, target_save_name)  # image name with index + "_target"
+
+            transformed_source.save(source_save_path)  # gives attribute error here but this works for PIL images
+            transformed_target.save(target_save_path)
+
+            pbar.update(1)
+            index += 1
+            if index > total_imgs:
+                break
+    pbar.close()
+
+
+if __name__ == '__main__':
+    input_dir = "C:/Users/eva_n/OneDrive - The University of Texas at Austin/SANDIA PHD RESEARCH/Ryan-AFM-Data/Combined-HS20MG-256/processed_pngs"
+    aug_dir = "C:/Users/eva_n/OneDrive - The University of Texas at Austin/PyCharm Projects/emn-n2n-pytorch/hs_20mg_train_data"
+    # augment(input_dir, aug_dir, 256, 1200)
+
+    source_in_dir = "C:/Users/eva_n/OneDrive - The University of Texas at Austin/SANDIA PHD RESEARCH/Ryan-AFM-Data/Combined-HS20MG-256/png_files"
+    target_in_dir = "C:/Users/eva_n/OneDrive - The University of Texas at Austin/SANDIA PHD RESEARCH/Ryan-AFM-Data/Combined-HS20MG-256/processed_pngs"
+    source_out_dir = "C:/Users/eva_n/OneDrive - The University of Texas at Austin/PyCharm Projects/emn-n2n-pytorch/hs_20mg_train_data"
+    target_out_dir = "C:/Users/eva_n/OneDrive - The University of Texas at Austin/PyCharm Projects/emn-n2n-pytorch/hs_20mg_train_data/targets"
+    number = 1200
+    px = 256
+
+    augment_pairs(source_in_dir, source_out_dir, target_in_dir, target_out_dir, number, px)
--- split.py
+++ split.py
@@ -4,19 +4,27 @@ import random
 import shutil
 
 ratio = 0.8
-root_dir = "new_ml_data"
-all_files = os.listdir(root_dir)
+root_dir = "hs_20mg_train_data/"
+all_files = [f for f in os.listdir(root_dir) if os.path.isfile(os.path.join(root_dir, f))]
 print(len(all_files), " total files")
 num_train = int(ratio * len(all_files))
 random.shuffle(all_files)
 train = all_files[:num_train]
 valid = all_files[num_train:]
 
-for img in train:
-    shutil.move(os.path.join(root_dir, img), "train\\" + img)
+train_dir = os.path.join(root_dir, "train")
+valid_dir = os.path.join(root_dir, "valid")
+if not os.path.isdir(train_dir):
+    os.mkdir(train_dir)
+if not os.path.isdir(valid_dir):
+    os.mkdir(valid_dir)
 
+for img in train:
+    file = os.path.join(root_dir, img)
+    shutil.move(file, os.path.join(train_dir, img))
 for img in valid:
-    shutil.move(os.path.join(root_dir, img), "valid\\" + img)
+    file = os.path.join(root_dir, img)
+    shutil.move(file, os.path.join(valid_dir, img))
 
 print(len(train), " files in train")
 print(len(valid), " files in valid")
