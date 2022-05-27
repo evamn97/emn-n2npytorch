92a4a8e8f8d0205fd7c7ea79b7a358692b925060
eva_n <eva.natinsky@utexas.edu>
2022-05-27 10:09:53 -0600

    add support for target pairs

---
+++ batch-script-files/03-n2npt-train.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# ----------------------------------------------------
+# Last revised: 19 May 2022
+# eva_mn
+# ----------------------------------------------------
+
+#SBATCH -J 03-n2npt-train         				        # Job name
+#SBATCH -o ../job-out-files/%x-%j.out                	# Name of stdout output file
+#SBATCH -p gtx          						        # Queue (partition) name
+#SBATCH -N 1               						        # Total # of nodes
+#SBATCH --ntasks-per-node 16							# Total # of tasks per node (mpi tasks)
+#SBATCH -t 00:10:00        						        # Run time (hh:mm:ss)
+#SBATCH --mail-user=eva.natinsky@austin.utexas.edu      # address to send notification emails
+#SBATCH --mail-type=all    						        # Send email at begin and end of job
+#SBATCH -A Modeling-of-Microsca					        # Allocation name (req'd if you have more than 1)
+
+start=$(date +%s)
+
+# !!!-------------------- SET INPUT VARS -----------------------------!!!
+data_dir="afm_data"
+noise="gradient"
+param=0.4
+reporting_int=100   # must be divisible by nbatches per epoch: nbatches = ntrain/batch-size
+ckpt_save="ckpts"
+# -----------------------------------------------------------------------
+
+echo "Date:  $(date)"
+
+module list
+
+# get job number and name for file saving in python script, set some params
+set -a
+jobid=${SLURM_JOBID}
+jobname=${SLURM_JOB_NAME}
+set +a    # only need to export SLURM vars
+
+# get from SLURM env vars
+echo -e "Begin batch job... \"${SLURM_JOB_NAME}\", #${SLURM_JOB_ID}\n"
+echo -e "Output file: ${SLURM_JOB_NAME}-${SLURM_JOB_ID}.out \nPartition: ${SLURM_JOB_PARTITION} \tNodes: ${SLURM_JOB_NUM_NODES} \tNtasks per node: ${SLURM_NTASKS}"
+
+# !!! ----------------------------- UPDATE THESE FOR CORRECTNESS ----------------------------- !!!
+echo -e "Dataset: TGX square pillars (22 imgs), 400/100/19 (train/val/test)"
+# ------------------------------------------------------------------------------------------------
+
+cd ..
+echo -e "Working directory:  $(pwd)\n"
+
+
+# Launch code using pipenv virtual environment
+pdm run python src/train.py \
+  -t ${data_dir}/train/ \
+  -v ${data_dir}/valid/ \
+  -n ${noise} \
+  -p ${param} \
+  --report-interval ${reporting_int} \
+  --ckpt-save-path ${ckpt_save} \
+  --ckpt-overwrite \
+  --cuda \
+  --plot-stats \
+  --clean-targets
+
+
+end=$(date +%s)
+runtime_minutes=$(((end-start)/60))
+runtime_seconds=$(((end-start)%60))
+echo "Batch job runtime was ${runtime_minutes} minutes and ${runtime_seconds} seconds."
