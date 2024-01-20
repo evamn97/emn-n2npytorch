fixed gradient noise param treatment + changed plot titles

src/datasets.py
@@ -110,7 +110,7 @@ class NoisyDataset(Dataset):
             noisy_img = tvF.resize(tvF.resize(img.to(torch.float64, copy=True), [hN, wN]), [h, w])
 
         elif self.noise_type.lower() == 'gradient':
-            std = rng.uniform(0, param) * img.std()
+            std = param * img.std()
             noise = torch.from_numpy(np.tile(np.linspace(0, 1, w), (h, 1))).unsqueeze(dim=0) * NormalDist(0, std).sample(img.size())
             noisy_img = img.to(torch.float64, copy=True) + noise
             if not img.is_floating_point():     # assumes int type means it's an image format (e.g., 0-255 range), so need rescale after summing

src/utils.py
@@ -147,7 +147,7 @@ def create_montage(img_name, noise_type, noise_param, save_path, source_t, denoi
     ssim_vals = [SSIM(np.asarray(source), np.asarray(clean)), SSIM(np.asarray(denoised), np.asarray(clean))]
     titles = ['Input: {:.2f} dB'.format(psnr_vals[0]),
               'Denoised: {:.2f} dB'.format(psnr_vals[1]),
-              'Ground truth']
+              'Clean target']
     zipped = zip(titles, [source, denoised, clean])
     for j, (title, img) in enumerate(zipped):
         ax[j].imshow(img, cmap='gray')  # cmap for height fields (not normalized)
@@ -164,7 +164,7 @@ def create_montage(img_name, noise_type, noise_param, save_path, source_t, denoi
     fname = os.path.splitext(img_name)[0]
     with open(os.path.join(save_path, 'metrics.csv'), 'a') as f:
         f.write(
-            f'{fname},{round(psnr_vals[0].item(), 2)},{round(psnr_vals[1].item(), 2)},{round(ssim_vals[0], 2)},{round(ssim_vals[1], 2)}\n')
+            f'{fname},{round(psnr_vals[0].item(), 2)},{round(psnr_vals[1].item(), 2)},{round(ssim_vals[0], 4)},{round(ssim_vals[1], 4)}\n')
 
     if not montage_only:
         source.save(os.path.join(save_path, f'{fname}-{noise_type}{noise_param}-noisy.png'))